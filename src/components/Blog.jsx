65c8bf39062d9bfa35c270736d8bb62e00569284
nsikakowo <owonsikak@gmail.com>
2022-07-11 23:37:59 +0100

    added basic css and loading screens

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -7,35 +7,39 @@ const BASE_API_URL = "https://dummyjson.com/"
 
 const Post =({text, title, image, link})=>{
     return(
-        <section className='my-[30px]'>
-        <h1 className='text-[30px]' >{title}</h1>
-        <div>
-            <img src={image} alt={title} />
+        <section className='my-[30px] mx-[10px] w-[450px] flex flex-col items-center bg-[#f7f7f7] rounded-[4px] pb-3 bShadow'>
+        <div className='w-[300px] h-[300px] mt-[30px]'>
+            <img src={image} alt={title} className="object-cover w-[100%] h-[100%]" />
         </div>
-        <p>{text}</p>
-        <Link to={`/technews/${link}`} >Read more... </Link>
+        <h1 className='text-[25px] font-bold my-[20px]' >{title}</h1>
+        <p className='w-[90%] text-center' >{text}</p>
+        <Link to={`/technews/${link}`} className='text-[#FFB72B] hover:font-bold duration-[2000ms] hover:text-[#B5FE83]' >Read more... </Link>
     </section>
     )
 }
 
 const Blog = () => {
-    const [blogPost, setblogPost] = useState([])
+    const [blogPost, setblogPost] = useState([]);
+    const [isLoading, setisLoading] = useState(false);
 
     useEffect(() => {
+        setisLoading(true);
         axios.get(BASE_API_URL + 'products/')
         .then((res)=>{
             console.log(res)
             const allPost = res.data.products;
             setblogPost(allPost)
+            setisLoading(false)
         })
         .catch((error)=>{
-            console.log(error)
+            console.log(error);
+            setisLoading(false)
         })
     }, [])
 
 
   return (
-    <section>
+    <section className='w-full flex flex-wrap justify-center'>
         {
             blogPost.map(post => (
                 <Post 
@@ -46,6 +50,15 @@ const Blog = () => {
                     link={post.id}
                 />
             ))
+        }
+        {
+            isLoading && 
+            <div>
+                <div className='loadbg'></div>
+                <div className='loadbx'>
+                <div className='ring'></div>
+                </div>
+            </div>
         }
     </section>
   )
--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,31 +1,50 @@
 import axios from 'axios'
 import {useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 const SinglePost = () => {
     const {id} = useParams();
 
     const [article, setarticle] = useState({});
-    const [photos, setphotos] = useState([])
+    const [photos, setphotos] = useState([]);
+    const [isLoading, setisLoading] = useState(false);
+
 
     useEffect(() => {
+        setisLoading(true)
         axios.get(`https://dummyjson.com/products/${id}`)
         .then((res)=>{
             console.log(res.data)
             setarticle(res.data)
-            setphotos(res.data.images)
+            setphotos(res.data.images);
+            setisLoading(false)
 
         })
         .catch((error)=>{
-            console.log(error)
+            console.log(error);
+            setisLoading(false)
         })
     }, [id])
     
 
 
   return (
-    <section>
-        <img src={article.thumbnail} alt="" />
+    <section className='py-7 flex flex-col items-center '>
+        {
+            isLoading ?
+            <div>
+                <div className='loadbg'></div>
+                <div className='loadbx'>
+                <div className='ring'></div>
+                </div>
+            </div>
+            :
+        
+        <>
+        <div className=' px-2'>
+            <div className='w-full h-[450px]'>
+                 <img src={article.thumbnail} alt="" className="object-cover w-[100%] h-[100%]"/>
+            </div>
         <div className='flex my-[10px]'>
             {
                 photos.map(img=> (
@@ -36,14 +55,38 @@ const SinglePost = () => {
                 ))
             }
         </div>
-        <h1>{article.title}</h1>
-        <p>BRAND: {article.brand} </p>
-        <h4>Category: {article.category}</h4>
-        <h5>Price: {article.price}</h5>
-        <h5>Discount: {article.discountPercentage}</h5>
-        <p>IN STOCK: {article.stock} </p>
-        <p>RATING: {article.rating}</p>
-        <p>Description: {article.description}</p>
+        <h1 className='text-5 font-bold my-2'>{article.title}</h1>
+        <div className='flex w-full justify-between'>
+            <p><span className='font-bold'>BRAND: </span>{article.brand} </p>  
+            <p><span className='font-bold text-[#5FD068]'>IN STOCK: </span>{article.stock} </p>
+            <h4 className='capitalize'><span className='font-bold text-[#FFBC97]'>Category: </span>{article.category}</h4>
+        </div>
+        <h5 className='my-2'>
+        <span className='font-bold'>Price: </span>
+            {article.price}
+        </h5>
+
+        <h5 className='my-2'>
+        <span className='font-bold'>Discount: </span>
+            {article.discountPercentage}
+        </h5>
+
+        <p className='my-2'>
+        <span className='font-bold'>RATING: </span>
+            {article.rating}
+        </p>
+
+        <p className='w-[90%] my-3 text-3'>
+            <span className='font-bold'>Description: </span>
+            {article.description}
+        </p>
+
+        </div>
+        <Link to="/">
+            <button className='py-1 px-2 cursor-pointer m-3 bg-[#FFBC97] rounded-[4px] ' >Back to All Post</button>
+        </Link>
+        </>
+}
     </section>
   )
 }
